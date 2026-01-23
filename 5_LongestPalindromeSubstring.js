7e7fb44cc7a7ad37fd8dc254d58278bf0fc7b4c4
AMohammedShafeek <amdshafeek7@gmail.com>
2026-01-23 09:51:58 +0530

    5. Longest Palinrome Substring

---
+++ 5_LongestPalindromeSubstring.js
@@ -0,0 +1,25 @@
+var longestPalindrome = function(s) {
+    if(s.length<2){
+        return s
+    }
+    let start = 0
+    let maxLength = 1
+    function expand(left, right){
+        while(left>=0 && right<s.length && s[left]===s[right]){
+            const len = right - left + 1
+            if(len > maxLength){
+                start = left
+                maxLength = len
+            }
+            left--
+            right++
+        }
+    }
+    for(let i=0; i<s.length; i++){
+        expand(i,i)
+        expand(i,i+1)
+    }
+    return s.substring(start, start+maxLength)
+};
+
+console.log(longestPalindrome("malayalan"));
